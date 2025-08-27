284f46c942c4f27e74d2a73b6d4ee0af86f4dafc
Martin Calderon <martin.calderon.ing@gmail.com>
2025-08-27 11:15:26 -0500

    Second commit

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {Profile} from '../profile/profile';
+import {Experience} from '../experience/experience';
+import {Projects} from '../projects/projects';
+import {Testimonials} from '../testimonials/testimonials';
+import {Education} from '../education/education';
+import {Articles} from '../articles/articles';
+import {Subscribe} from '../subscribe/subscribe';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [
+    Profile,
+    Experience,
+    Projects,
+    Testimonials,
+    Education,
+    Articles,
+    Subscribe
+  ],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
