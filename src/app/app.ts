4081a07c2a39e2d12e879e80628d503f24e20034
Martin Calderon <martin.calderon.ing@gmail.com>
2025-08-27 00:05:26 -0500

    First Commit

---
+++ src/app/app.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import {Header} from './components/header/header';
+import {Content} from './components/content/content';
+import {Footer} from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    Header,
+    Content,
+    Footer
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('portfolio-app');
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => window.HSStaticMethods.autoInit(), 100);
+      }
+    });
+  }
+}
